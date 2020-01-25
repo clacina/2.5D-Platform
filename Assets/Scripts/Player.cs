893d2714f7af2b97fa42af6a1a647082a5b7b01b
Chris Lacina <clacina@flowroute.com>
2020-01-25 14:44:39 -0800

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController _controller;
+
+    [SerializeField]
+    private float _character_speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Horizontal input
+        float horizontal_movement = Input.GetAxis("Horizontal");    // -1 ... +1
+        Vector3 direction = new Vector3(horizontal_movement, 0, 0);
+
+        // Now move based on that direction.
+        _controller.Move(direction * Time.deltaTime * _character_speed);
+    }
+}
