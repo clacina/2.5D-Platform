de6880e747292680c57ddfb2c3e6daa67815d0a9
Chris Lacina <clacina@flowroute.com>
2020-01-25 15:12:52 -0800

    Gravity

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,12 @@ public class Player : MonoBehaviour
 
     [SerializeField]
     private float _character_speed = 5.0f;
+    [SerializeField]
+    private float _gravity = 1.0f;
+
+    [SerializeField]
+    private float _jumpHeight = 15.0f;
+    private float _yVelocity;
 
     // Start is called before the first frame update
     void Start()
@@ -21,8 +27,23 @@ public class Player : MonoBehaviour
         // Horizontal input
         float horizontal_movement = Input.GetAxis("Horizontal");    // -1 ... +1
         Vector3 direction = new Vector3(horizontal_movement, 0, 0);
+        Vector3 velocity = direction * _character_speed;
 
         // Now move based on that direction.
-        _controller.Move(direction * Time.deltaTime * _character_speed);
+
+        // if grounded, do nothing, else, apply gravity
+        if(!_controller.isGrounded)
+        {
+            _yVelocity -= _gravity;
+        } else // Jump, Jump!!
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _yVelocity += _jumpHeight;
+            }
+        }
+        velocity.y = _yVelocity;
+
+        _controller.Move(velocity * Time.deltaTime);
     }
 }
